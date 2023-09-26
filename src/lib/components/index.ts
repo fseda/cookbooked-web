d6a68c8dbd05d80153bb48699e0328ae6c7e1710
fseda <felipeseda@poli.ufrj.br>
2023-09-26 15:16:28 -0300

    Export password components

---
+++ src/lib/components/index.ts
@@ -0,0 +1,3 @@
+export { default as PasswordInput } from './Inputs/Passwords/PasswordInput.svelte';
+export { default as PasswordSignup } from './Inputs/Passwords/PasswordSignup.svelte';
+export { password, passwordConfirm, passwordsMatch } from './Inputs/Passwords/stores'
