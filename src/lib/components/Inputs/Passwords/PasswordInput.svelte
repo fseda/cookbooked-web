10e5cc2332ba2d1e3489bff4243a938393544a09
fseda <felipeseda@poli.ufrj.br>
2023-09-26 15:15:32 -0300

    Add component for password login

---
+++ src/lib/components/Inputs/Passwords/PasswordInput.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { Label, Input } from 'flowbite-svelte';
+  import { EyeOutline, EyeSlashOutline } from 'flowbite-svelte-icons';
+  import { password } from '$lib/components';
+
+  let showPassword: boolean = false;
+
+  function togglePasswordVisibility(): void {
+    showPassword = !showPassword;
+  }
+
+</script>
+
+<Label class="space-y-2" for="password-confirm">
+  <span>Password</span>
+  <Input 
+    id="password-confirm"
+    type={ showPassword ? "text" : "password" } 
+    name="password-confirm" 
+    required
+    bind:value={$password}
+  >
+    <button type="button" slot="left" on:click={togglePasswordVisibility} class="pointer-events-auto">
+      {#if showPassword}
+        <EyeOutline class="w-6 h-6" />
+      {:else}
+        <EyeSlashOutline class="w-6 h-6" />
+      {/if}
+    </button>
+  </Input>
+</Label>
