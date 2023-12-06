7340f911b23a57855f78df9a009d6096105f35a4
fseda <felipeseda@poli.ufrj.br>
2023-12-05 23:37:21 -0300

    disable zoom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,30 @@
 	import Footer from "$lib/components/layout/Footer.svelte";
 	import Header from "$lib/components/layout/Header.svelte";
 	import { isLoggedIn } from "$lib/stores/user";
+	import { onMount } from "svelte";
   import "../app.css";
   import "@picocss/pico";
+	import { browser } from "$app/environment";
 
   export let data;
   $: isLoggedIn.set(!!data.token);
+
+  const handleTouchMove = (e: TouchEvent) => {
+    const touchEvent = e as TouchEvent & { scale?: number };
+    if (touchEvent.scale && touchEvent.scale !== 1) {
+      e.preventDefault();
+    }
+  }
+
+  onMount(() => {
+    if (browser) 
+      window.addEventListener('touchmove', handleTouchMove, { passive: false });
+
+      return () => {
+        if (browser) 
+          window.removeEventListener('touchmove', handleTouchMove);
+      }
+  })
 </script>
 
 <Header/>
