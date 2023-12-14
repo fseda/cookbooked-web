1be3dfeb949e56363fce959fc41c20aa55aa5a0b
fseda <felipeseda@poli.ufrj.br>
2023-12-14 00:49:22 -0300

    icons for github

---
+++ src/lib/components/github/GitHubLogo.svelte
@@ -0,0 +1,19 @@
+<script>
+  import GHL from '$lib/assets/github/github-logo.png';
+  import GHLW from '$lib/assets/github/github-logo-white.png';
+  import { theme } from '$lib/stores/theme';
+
+  export let reversed = false;
+</script>
+
+{#if reversed}
+  <img src={$theme === 'dark' ? GHL : GHLW} alt="GitHub Logo" class="github-logo">
+{:else}
+  <img src={$theme === 'dark' ? GHLW : GHL} alt="GitHub Logo" class="github-logo">
+{/if}
+
+<style>
+  .github-logo {
+    width: 3.5rem;
+  }
+</style>
